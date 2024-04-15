b93398f645b2870b5e8699ee89996ba231cea04e
Junshan Liu <junshan_liu@esri.com>
2024-04-15 16:44:06 +0800

    locale and resource

--- components/map-react/src/app.tsx
+++ components/map-react/src/app.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { ArcgisMap, ArcgisSearch, ArcgisLegend } from "@arcgis/map-components-react";
+import * as esriIntl from '@arcgis/core/intl'
 
+esriIntl.setLocale('zh-cn')
 export function App() {
   return <React.StrictMode>
     <ArcgisMap item-id="d5dda743788a4b0688fe48f43ae7beb9">
