e48b996233f8b73e7be93ffdf07e85af677a5b03
Junshan Liu <junshan_liu@esri.com>
2024-04-16 10:49:35 +0800

    load assets

--- components/map-react/src/app.tsx
+++ components/map-react/src/app.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { ArcgisMap, ArcgisSearch, ArcgisLegend } from "@arcgis/map-components-react";
 import * as esriIntl from '@arcgis/core/intl'
+import esriConfig from "@arcgis/core/config"
 
 esriIntl.setLocale('zh-cn')
+esriConfig.assetsPath = "https://rqawinbi01pt.ags.esri.com/gis/jsapi/jsapi4/@arcgis/core/assets"
+
 export function App() {
   return <React.StrictMode>
     <ArcgisMap item-id="d5dda743788a4b0688fe48f43ae7beb9">
--- components/map-react/src/index.tsx
+++ components/map-react/src/index.tsx
@@ -6,7 +6,7 @@ import "@arcgis/core/assets/esri/themes/dark/main.css";
 // import defineCustomElements to register custom elements with the custom elements registry
 import { defineCustomElements } from "@arcgis/map-components/dist/loader";
 // Register custom elements
-defineCustomElements(window, { resourcesUrl: "https://rqawinbi01pt.ags.esri.com/gis/jsapi/jsapi4/map-components/4.29/assets" });
+defineCustomElements(window);
 
 const root = ReactDOM.createRoot(document.getElementById("app") as HTMLElement);
 root.render(
