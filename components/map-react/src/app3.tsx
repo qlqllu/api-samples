24e56720126001d5a7df560dd47f6f7f2e4af086
Junshan Liu <junshan_liu@esri.com>
2024-10-16 13:29:24 +0800

    update

--- components/map-react/src/app3.tsx
+++ components/map-react/src/app3.tsx
@@ -5,32 +5,27 @@ import esriConfig from "@arcgis/core/config"
 
 esriIntl.setLocale('zh-cn')
 // esriConfig.assetsPath = "https://rqawinbi01pt.ags.esri.com/gis/jsapi/jsapi4/@arcgis/core/assets"
-esriConfig.assetsPath = "https://js.arcgis.com/4.29/@arcgis/core/assets"
+esriConfig.assetsPath = "https://jsdev.arcgis.com/4.31/@arcgis/core/assets"
 
 /**
- * Use onArcgisViewReadyChange and onArcgisLegendReady.
+ * Use onArcgisViewReadyChange
  */
 export function App() {
   const [view, setView] = React.useState<any>(null)
+  const mapRef = React.useRef<any>(null)
 
+  // This is required to re-render the legend when the view changes.
   const onArcgisViewReadyChange = (event: CustomEvent) => {
     if (!event.target) {
       return
     }
     setView((event.target as any).view)
   }
-  const onArcgisLegendReady = (event: CustomEvent) => {
-    if (!event.target) {
-      return
-    }
-
-    (event.target as any).view = view
-  }
 
   return <React.StrictMode>
-    <ArcgisMap item-id="d5dda743788a4b0688fe48f43ae7beb9" onArcgisViewReadyChange={onArcgisViewReadyChange}>
+    <ArcgisMap ref={mapRef} item-id="d5dda743788a4b0688fe48f43ae7beb9" onArcgisViewReadyChange={onArcgisViewReadyChange}>
     </ArcgisMap>
 
-    {view && <ArcgisLegend onArcgisLegendReady={onArcgisLegendReady}></ArcgisLegend>}
+    {<ArcgisLegend style={{position: 'absolute', bottom: 10, left: 10}} referenceElement={mapRef.current}></ArcgisLegend>}
   </React.StrictMode>
 }
--- components/map-react/src/app4.tsx
+++ components/map-react/src/app4.tsx
@@ -8,13 +8,22 @@ esriIntl.setLocale('zh-cn')
 esriConfig.assetsPath = "https://jsdev.arcgis.com/4.31/@arcgis/core/assets"
 
 /**
- * Put components inside the ArcgisMap component.
+ * Use onArcgisViewReadyChange
  */
 export function App() {
+  const legendRef = React.useRef<any>(null)
+
+  const onArcgisViewReadyChange = (event: CustomEvent) => {
+    if (!event.target || !legendRef.current) {
+      return
+    }
+    legendRef.current.view = (event.target as any).view
+  }
+
   return <React.StrictMode>
-    <ArcgisMap item-id="d5dda743788a4b0688fe48f43ae7beb9">
-      <ArcgisSearch position="top-right"></ArcgisSearch>
-      <ArcgisLegend position="bottom-left"></ArcgisLegend>
+    <ArcgisMap item-id="d5dda743788a4b0688fe48f43ae7beb9" onArcgisViewReadyChange={onArcgisViewReadyChange}>
     </ArcgisMap>
+
+    {<ArcgisLegend ref={legendRef} style={{position: 'absolute', bottom: 10, left: 10}}></ArcgisLegend>}
   </React.StrictMode>
 }
